4ac7c6d12f94638c495280e01868137f702c4a85
optclblast <molodoy.p4elovek@yandex.ru>
2023-06-17 20:41:53 +0300

    bubblesort

--- bubblesort.go
+++ bubblesort.go
@@ -10,14 +10,20 @@ func main() {
 	var lng int
 	fmt.Print("Slice size: ")
 	fmt.Scan(&lng)
+
 	rnd := randomSlice(lng)
+
+	startTime := time.Now()
+
 	fmt.Printf("Src slice: %v\n", rnd)
 
 	fmt.Printf("Sorted slice: %v\n", sortTheSlice(rnd))
+	runtime := time.Since(startTime)
+	fmt.Printf("Runtime: %v%v%v", runtime.Minutes(), runtime.Seconds(), runtime.Milliseconds())
+	fmt.Scan(&lng)
 }
 
 func sortTheSlice(s []int) []int {
-	//sortedSlice := make([]int, len(s))
 	for i := 0; i < len(s)-1; i++ {
 		for j := 0; j < len(s)-i-1; j++ {
 			if s[j] > s[j+1] {
